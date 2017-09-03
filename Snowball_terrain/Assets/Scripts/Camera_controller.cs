e7709c488d44cf65fd85b72df91c6240057fef95
MarkH-PC\MarkH <mdh326@drexel.edu>
2017-09-03 07:42:23 -0400

    Building Mats Added, Building Colliders Added, Building Prefabs Redone, Simple Camera Change for End (function needs to be called for our End Trigger)

--- Snowball_terrain/Assets/Scripts/Camera_controller.cs
+++ Snowball_terrain/Assets/Scripts/Camera_controller.cs
@@ -18,6 +18,11 @@ public class Camera_controller : MonoBehaviour {
     private Vector3 cameraTarget;   //Where camera should look
     public float cTargetyOffset;    //how high above character to look
 
+    private bool cameraToEnd = false;   //variables for camera reposition at end
+    public Transform endCameraTarg;
+    public Transform endCameraPos;
+    public float endSpeed;
+
     void Start ()
     {
         minDistance = Vector3.Distance(this.transform.position, player.transform.position);
@@ -34,19 +39,36 @@ public class Camera_controller : MonoBehaviour {
 	// Update is called once per frame
 	void LateUpdate ()
     {
-        //Player_controller s_controller = player.GetComponent<Player_controller>();
-        float scaleRate = player.transform.localScale.y;
+        //for testing -->  if (Input.GetKeyDown("return")) CameraSetToEnd();
 
-        distance = minDistance + scaleRate;
+        if (!cameraToEnd) {
+            //Player_controller s_controller = player.GetComponent<Player_controller>();
+            float scaleRate = player.transform.localScale.y;
+
+            distance = minDistance + scaleRate;
         
-        transform.position = (offset).normalized * distance + player.transform.position;
+            transform.position = (offset).normalized * distance + player.transform.position;
 
-        //Debug.Log(distance);
-        //transform.position = player.transform.position + offset;
-        //offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offset;
-        //transform.position = player.transform.position + offset;
+            //Debug.Log(distance);
+            //transform.position = player.transform.position + offset;
+            //offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offset;
+            //transform.position = player.transform.position + offset;
 
-        cameraTarget = new Vector3(player.transform.position.x, player.transform.position.y + cTargetyOffset, player.transform.position.z);
-        transform.LookAt(cameraTarget);
+            cameraTarget = new Vector3(player.transform.position.x, player.transform.position.y + cTargetyOffset, player.transform.position.z);
+            transform.LookAt(cameraTarget);
+
+
+        //for camera reposition at end of track
+        } else {
+            transform.position = Vector3.MoveTowards(transform.position, endCameraPos.position, endSpeed * Time.deltaTime);
+            transform.LookAt(endCameraTarg);
+        }
+    }
+
+    // use function when snowball enters end trigger or however we decide end
+    public void CameraSetToEnd() {
+        transform.parent = null;
+        cameraToEnd = true;
+        transform.GetChild(1).gameObject.SetActive(false); // !!! fog needs to disappear slowly, be deparented from camera or disabled (as is the current solution) 
     }
 }
